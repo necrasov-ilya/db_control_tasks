2b350bd7848d1ba8e3749e78ad67a53598f837f3
NKSV_ILYA <evart2006@gmail.com>
2025-11-19 12:08:59 +0500

    Migrate Clients to surrogate key and update schema
    Replaces natural key Client_Name with surrogate key Client_ID in the Accounts table

--- kr3/002_insert_data.sql
+++ kr3/002_insert_data.sql
@@ -1,9 +1,7 @@
--- Добавляем клиентов
 INSERT INTO Clients (Client_Name, Client_Address) VALUES
     ('ООО "Восток"', 'Москва, ул. Мира, 1'),
     ('ИП Ларин', 'Санкт-Петербург, Невский пр., 10');
 
--- Добавляем банковские счета
 INSERT INTO Accounts (Account_ID, Account_Number, Currency, Client_Name) VALUES
     ('ACC01', '4070281000000001', 'RUB', 'ООО "Восток"'),
     ('ACC02', '4070281000000002', 'USD', 'ООО "Восток"'),
